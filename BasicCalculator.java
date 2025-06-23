d6183a153932b2c0b8f75473d61e29177b8b5381
M Vishnu Vardhan Reddy <117419935+Vishnureddy630@users.noreply.github.com>
2025-06-23 18:50:17 +0530

    BasicCalculator.java

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -2,35 +2,23 @@ import java.util.Scanner;
 class BasicCalculator
 {
 static Scanner sc= new Scanner(System.in);
-static int a=0,b=0;
+static float a=0,b=0;
 public static void add()
 {
-		System.out.println("Enter the First value: ");
-		a=sc.nextInt();
-		System.out.println("Enter the Second value: ");
-		b=sc.nextInt();
+		
 		System.out.println("Additon of two numbers are :"+(a+b));
 }
 public static void sub()
 {
-		System.out.println("Enter the First value: ");
-		a=sc.nextInt();
-		System.out.println("Enter the Second value: ");
-		b=sc.nextInt();
+		
 		System.out.println("Subtraction of two numbers are :"+(a-b));
 }public static void mult()
 {
-		System.out.println("Enter the First value: ");
-		a=sc.nextInt();
-		System.out.println("Enter the Second value: ");
-		b=sc.nextInt();
+
 		System.out.println("Multiplication of two numbers are :"+(a*b));
 }public static void div()
 {
-		System.out.println("Enter the First value: ");
-		a=sc.nextInt();
-		System.out.println("Enter the Second value: ");
-		b=sc.nextInt();
+		
 		if(b==0)
 		{
 			System.out.println("Cannot divide by zero");
@@ -39,6 +27,14 @@ public static void sub()
 		System.out.println("Division of two numbers are :"+(a/b));
 
 }
+public static void input()
+{
+    System.out.println("Enter the First value: ");
+	a=sc.nextFloat();
+	System.out.println("Enter the Second value: ");
+	b=sc.nextFloat();
+    
+}
 public static void main(String[] args)
 	{
 
@@ -46,28 +42,37 @@ public static void main(String[] args)
 
 		while(true)
 {
-		System.out.println("Press + for addition, - for  subtraction, * for  multiplication, / for  division, # for exit");	
+		System.out.println("Press + for addition, - for  subtraction, * for  multiplication, / for  division, 0 for terminated");	
 		char operation=sc.next().charAt(0);
+		
 		switch(operation)
 {
 	case '+':
+	    input();
 		add();
 		break;
 
 	case '-':
+	    input();
 		sub();
 		break;
 
 	case '*':
+	    input();
 		mult();
 		break;
 
 	case '/':
+	    input();
 		div();
 		break;
-	case '#':
+	case '0':
+	    System.out.println("Program is terminated...");
 		sc.close();
 		System.exit(0);
+	default :
+	    System.out.println("Please enter the valid symbol");
+	 
 }
 }
 	}
