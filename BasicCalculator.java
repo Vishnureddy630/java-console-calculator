8551dfc1196955499ac99a7d1984918078901414
M Vishnu Vardhan Reddy <117419935+Vishnureddy630@users.noreply.github.com>
2025-06-23 20:08:03 +0530

    Updated BasicCalculator.java

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
-class BasicCalculator
+class  BasicCalculator
 {
 static Scanner sc= new Scanner(System.in);
-static float a=0,b=0;
-public static void add()
+
+public static void add(double a,double b)
 {
 		
-		System.out.println("Additon of two numbers are :"+(a+b));
+		System.out.println("Additon of two numsbers are :"+(a+b));
 }
-public static void sub()
+public static void sub(double a,double b)
 {
 		
-		System.out.println("Subtraction of two numbers are :"+(a-b));
-}public static void mult()
+		System.out.println("Subtraction of two numsbers are :"+(a-b));
+}public static void mult(double a,double b)
 {
 
-		System.out.println("Multiplication of two numbers are :"+(a*b));
-}public static void div()
+		System.out.println("Multiplication of two numsbers are :"+(a*b));
+}public static void div(double a,double b)
 {
 		
 		if(b==0)
@@ -24,16 +24,40 @@ public static void sub()
 			System.out.println("Cannot divide by zero");
 			return;
 		}
-		System.out.println("Division of two numbers are :"+(a/b));
+		System.out.println("Division of two numsbers are :"+(a/b));
 
 }
-public static void input()
+public static double[] input()
 {
-    System.out.println("Enter the First value: ");
-	a=sc.nextFloat();
-	System.out.println("Enter the Second value: ");
-	b=sc.nextFloat();
     
+    double a,b;
+		while(true){
+        		try
+        		{
+        		    System.out.println("Enter the First value: ");
+        	        a=sc.nextDouble();
+        	    
+        	        break;
+        		}
+        		catch(Exception e){
+        		    System.out.println("Enter valid values "+e);
+        		     sc.nextLine();
+        		}
+		
+		}
+		
+		while(true){
+        		try{
+        	        System.out.println("Enter the Second value: ");
+        	        b=sc.nextDouble();
+        	        break;
+        		}
+        		catch(Exception e){
+        		    System.out.println("Enter valid values "+e);
+        		     sc.nextLine();
+        		}
+    		}
+		return new double[]{a,b};
 }
 public static void main(String[] args)
 	{
@@ -45,35 +69,30 @@ public static void main(String[] args)
 		System.out.println("Press + for addition, - for  subtraction, * for  multiplication, / for  division, 0 for terminated");	
 		char operation=sc.next().charAt(0);
 		
-		switch(operation)
-{
-	case '+':
-	    input();
-		add();
-		break;
-
-	case '-':
-	    input();
-		sub();
-		break;
-
-	case '*':
-	    input();
-		mult();
-		break;
-
-	case '/':
-	    input();
-		div();
-		break;
-	case '0':
-	    System.out.println("Program is terminated...");
-		sc.close();
+		if(operation=='0')
+		{
+		 System.out.println("Program is terminated...");
+	     sc.close();
 		System.exit(0);
-	default :
-	    System.out.println("Please enter the valid symbol");
-	 
-}
+		}
+		
+		
+	 switch (operation) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    double[] arr = input();
+                    switch (operation) {
+                        case '+': add(arr[0], arr[1]); break;
+                        case '-': sub(arr[0], arr[1]); break;
+                        case '*': mult(arr[0], arr[1]); break;
+                        case '/': div(arr[0], arr[1]); break;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operation. Please try again.");
+            }
 }
 	}
 }
